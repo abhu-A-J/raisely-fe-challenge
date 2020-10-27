ce90d47d3958d5431572ddd653cc4dd2f455abc5
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2020-10-27 11:59:52 +0530

    Addition of the herobox content component

--- src/components/HeroboxContent/index.jsx
+++ src/components/HeroboxContent/index.jsx
@@ -7,7 +7,12 @@ import "./index.scss";
 const HeroboxContent=()=>{
 	return (
 		<div className="herobox__content">
-			This is herobox Content!
+			<h1 className="main-heading">
+				Create your next fundraising campaign
+			</h1>
+			<p className="tagline">
+			Build stunning fundraising websites with Raisely to grow your cause, campaign, or charity for free.
+			</p>
 		</div>
 	)
 }
